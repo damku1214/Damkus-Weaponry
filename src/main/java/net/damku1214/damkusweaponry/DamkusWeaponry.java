7cc34830f4e0953acc1c7d2cf56d5671f159fded
damku1214 <kukeudamseu@gmail.com>
2023-07-03 10:32:07 -0400

    Added Jade Vine Sickle

--- src/main/java/net/damku1214/damkusweaponry/DamkusWeaponry.java
+++ src/main/java/net/damku1214/damkusweaponry/DamkusWeaponry.java
@@ -1,9 +1,11 @@
 package net.damku1214.damkusweaponry;
 
+import net.damku1214.damkusweaponry.item.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -26,8 +28,11 @@ public class DamkusWeaponry {
     private static final Logger LOGGER = LogManager.getLogger();
 
     public DamkusWeaponry() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
